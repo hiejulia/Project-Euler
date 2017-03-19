474c0ed7474c5f1a0049138df2195c056ce5670f
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-19 13:24:53 +0200

    pr48

---
+++ part1/pr48.js
@@ -0,0 +1,18 @@
+/**
+ * 1^1 + 2^2 + 3^3 +....+ 1000^1000
+ */
+
+function test(){
+    var sum =0;
+    for(var i=1;i<=1000;i++){
+        sum += Math.pow(i,i);
+    }
+    console.log(sum);
+    //sum
+    sum = sum.toString().split("");
+    for(var i=)
+}
+
+test();
+
+//to be continue... 
