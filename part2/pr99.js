4f9d5f1bed63caa2599f3b9189bc05eec9dba793
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-28 14:29:25 +0300

    cal with logarithm

---
+++ part2/pr99.js
@@ -0,0 +1,23 @@
+//solution: instead compare exponential, we will compare logariths
+
+
+
+(function(){
+    
+    function solution(n1,n2) {
+        var basenum;
+        var exponent;
+        basenum = Math.log(parseInt(n1));
+        exponent = parseInt(n2);
+        var number = basenum * exponent; 
+
+    }
+
+
+    solution(num1, num2);
+
+
+})();
+
+
+
