e1167b27e0f35dd96e98b4de71bcd0073be70a7e
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-28 13:41:09 +0300

    finish pr 48 :octocat:

--- part1/pr48.js
+++ part1/pr48.js
@@ -3,6 +3,8 @@
  */
 
 function test(){
+    var result=[];
+    
     var sum =0;
     for(var i=1;i<=1000;i++){
         sum += Math.pow(i,i);
@@ -10,7 +12,18 @@ function test(){
     console.log(sum);
     //sum
     sum = sum.toString().split("");
-    for(var i=)
+    for(var i=sum.length-1;i>=sum.length-11;i--) {
+        result.push(i);
+        console.log(result);
+        result.join("");
+        //result = ""
+        result = parseInt(result);
+
+        console.log(result);
+        
+
+
+    }
 }
 
 test();
