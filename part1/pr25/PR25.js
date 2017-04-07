1db71d087c5443600c05e1301eb9dc287a9d9312
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-07 13:11:33 +0300

    install big int via npm

---
+++ part1/pr25/PR25.js
@@ -0,0 +1,23 @@
+(function(){
+'use strict';
+//init 
+//idea :
+/**
+ * 1.tim day so fibonnaci 
+ * 2.cho vong while chay tu 1, > 0 
+ * 3.Bat dau thuat toan tim so fibonacci 
+ * 4.voi so fibonnaci tim dc trong vong lap while 
+ * 5.chay function tim so luong chu so, neu = 1000 => break ngay va in ra ket qua
+ * ======
+ * function tim so luong chu so: int => chuyen sang string => length 
+ * 
+ */
+
+//BRUTE FORCE SOUTION 
+
+
+
+
+
+
+})();
---
+++ part1/pr25/fibonacci.js
@@ -0,0 +1,1 @@
+var bigint = require('bigint');
