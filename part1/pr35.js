4c1c1e80abe7ca2ca8eef38567faf9df59938ffb
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-08 16:47:11 +0300

    solutions

--- part1/pr35.js
+++ part1/pr35.js
@@ -8,3 +8,14 @@ There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73
 How many circular primes are there below one million?
  */
 
+
+//get the list of primes number from eratosthenes from pr10
+
+//check if the number contain number even 0,2,4,6,8 or 5 => if yes, false => go back 1 =>
+//move the number to the temporary list 
+
+//rotate the number and check if prime  => if not => back 1=> if yes, move to a temporary list
+//return the length of the temporary list 
+
+
+
