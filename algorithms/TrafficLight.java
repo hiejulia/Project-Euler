796c7d58f3b681f3cb310c7b3fdf8936ebbba4ec
hiejulia <hienminhnguyen711@gmail.com>
2019-10-10 13:03:46 +0300

    add lesson

--- algorithms/TrafficLight.java
+++ algorithms/TrafficLight.java
@@ -2,64 +2,45 @@ import java.util.*;
 
 class TrafficLight {
 
-    // Design traffic light 
+    // Design traffic light
 
+    public static void main(String[] args) {
 
-
-
-
-    public static void main(String [] args) {
-
- 
     }
 
+    // Linear Search
 
-    // Linear Search 
+    // Binary Search
 
+    // Interpolation Search
 
+    // Smallest avg in an array partition
 
+    public static int smallestAvg(int[] A) {
 
-
-    // Binary Search 
-
-
-
-
-
-    // Interpolation Search 
-
-
-
-
-    // Smallest avg in an array partition 
-
-    public static int smallestAvg(int[] A){
-
-        int smallestStart =0;
+        int smallestStart = 0;
 
         double smallestAvgValue = Double.MAX_VALUE;
 
-        for(int i = 0; i< A.length -1; i++){
-            if( ((A[i] + A[i+1])/(double)2) < smallestAvgValue){
-                smallestAvgValue = ((A[i] + A[i+1])/(double)2);
+        for (int i = 0; i < A.length - 1; i++) {
+            if (((A[i] + A[i + 1]) / (double) 2) < smallestAvgValue) {
+                smallestAvgValue = ((A[i] + A[i + 1]) / (double) 2);
                 smallestStart = i;
             }
-            //System.out.println(smallestAvg + " vs " + ((A[i] + A[i+1])/2) + " " + smallestStart);
-            if((i+2)<A.length && ((A[i] + A[i+1] + A[i+2])/(double)3) < smallestAvgValue){
-                smallestAvgValue = ((A[i] + A[i+1] + A[i+2])/(double)3);
+            // System.out.println(smallestAvg + " vs " + ((A[i] + A[i+1])/2) + " " +
+            // smallestStart);
+            if ((i + 2) < A.length && ((A[i] + A[i + 1] + A[i + 2]) / (double) 3) < smallestAvgValue) {
+                smallestAvgValue = ((A[i] + A[i + 1] + A[i + 2]) / (double) 3);
                 smallestStart = i;
             }
         }
 
-
-
         return smallestStart;
     }
 
+    // Lesson 3: Time complexity
+    // FrogJump - count the minimal number of jumps from X - Y
 
-    // Lesson 3: Time complexity 
-    // FrogJump - count the minimal number of jumps from X - Y 
-    
     public static int frogJump(int X, int Y, int D)){
 
         int minJump  = 0;
@@ -77,16 +58,24 @@ class TrafficLight {
 
     }
 
+    /**
+     * Lesson 5 - Prefix sums
+     */
 
+    // Passing cars
 
+    // End passing cars
 
-
-
-    
-
+    int getPassingCars(int A[], int n) {
+        int result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (A[i] == 0) {
+                for (int j = i + 1; j < n; j++)
+                    if (A[j])
+                        result++;
+            }
+        }
+        return result;
+    }
 
 }
-
-
-
-
